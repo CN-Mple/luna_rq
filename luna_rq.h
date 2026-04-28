06d1b37cccffa1571cbcc6b2e7ddb33782b5d5b4
Purple Moon <2434717958@qq.com>
2026-04-28 10:37:26 +0800

    Add files via upload

--- luna_rq.h
+++ luna_rq.h
@@ -1,91 +1,110 @@
-/* luna_queue.h */
-#ifndef LUNA_QUEUE_H
-#define LUNA_QUEUE_H
+/* luna_rq.h */
+#ifndef LUNA_RQ_H
+#define LUNA_RQ_H
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
 #ifndef LUNA_ASSERT
 #include <assert.h>
-#define LUNA_ASSERT		assert
+#define LUNA_ASSERT			assert
 #endif
 
-struct queue {
-	uint32_t **ring;
-	uint32_t   end;
-	uint32_t   head;
-	uint32_t   tail;
-	uint32_t   nfree;
+#define LUNA_ALIGN_UP(x, align)		(((x) + (align) - 1) & ~((align) - 1))
+#define LUNA_ALIGN_DOWN(x, align)	((x) & ~((align) - 1))
+
+struct rq {
+	uint8_t  *buffer;
+	uint32_t  capacity;
+	uint32_t  elem_size;
+	uint8_t  *base;
+	uint8_t  *w;
+	uint8_t  *r;
+	uint8_t  *end;
 };
 
-void luna_queue_init(struct queue *queue, uint32_t *ring[], uint32_t len);
+void luna_rq_init(struct rq *rq, uint8_t *buffer, uint32_t capacity, uint32_t size);
 
-uint32_t *luna_queue_pop(struct queue *queue);
-void luna_queue_push(struct queue *queue, uint32_t *data);
+bool luna_rq_pop(struct rq *rq, uint8_t *data);
+bool luna_rq_push(struct rq *rq, const uint8_t *data);
 
-bool luna_queue_is_empty(struct queue *queue);
-bool luna_queue_is_full(struct queue *queue);
+bool luna_rq_is_empty(const struct rq *rq);
+bool luna_rq_is_full(const struct rq *rq);
 
 #endif
 
-#define LUNA_QUEUE_IMPLEMENTATION
-#ifdef LUNA_QUEUE_IMPLEMENTATION
+#ifdef LUNA_RQ_IMPLEMENTATION
 
-void luna_queue_init(struct queue *queue, uint32_t *ring[], uint32_t len)
+void luna_rq_init(struct rq *rq, uint8_t *buffer, uint32_t capacity, uint32_t size)
 {
-	LUNA_ASSERT(queue);
-	LUNA_ASSERT(ring);
-	LUNA_ASSERT(len >= 2);
+	LUNA_ASSERT(rq);
+	LUNA_ASSERT(buffer);
+	LUNA_ASSERT(size > 0);
+	LUNA_ASSERT(capacity > 0);
+	LUNA_ASSERT(size <= capacity);
 
-	queue->head  = 0;
-	queue->tail  = 0;
-	queue->end   = len;
-	queue->nfree = len;
-	queue->ring  = &ring[0];
+	rq->buffer      = buffer;
+	rq->capacity    = capacity;
+	rq->elem_size   = LUNA_ALIGN_UP(size, sizeof(uintptr_t));
+
+	rq->base        = LUNA_ALIGN_UP((uintptr_t)buffer, sizeof(uintptr_t));
+	rq->w = rq->r   = rq->base;
+	uint32_t number = (buffer + capacity - rq->base) / rq->elem_size;
+	rq->end         = rq->base + rq->elem_size * number;
 }
 
-uint32_t *luna_queue_pop(struct queue *queue)
+bool luna_rq_pop(struct rq *rq, uint8_t *data)
 {
-	LUNA_ASSERT(queue);
-
-	uint32_t *data = 0;
-	if (queue->nfree != queue->end) {
-		data = queue->ring[queue->tail];
-		if (queue->tail == 0) {
-			queue->tail = queue->end;
-		}
-		--queue->tail;
-
-		++queue->nfree;
-	}
-	return data;
-}
-
-void luna_queue_push(struct queue *queue, uint32_t *data)
-{
-	LUNA_ASSERT(queue);
+	LUNA_ASSERT(rq);
 	LUNA_ASSERT(data);
 
-	LUNA_ASSERT(queue->nfree != 0);
-	queue->ring[queue->head] = data;
-	if (queue->head == 0) {
-		queue->head = queue->end;
+	if (luna_rq_is_empty(rq)) {
+		return false;
 	}
-	--queue->head;
 
-	--queue->nfree;
+	uint8_t *src = rq->r;
+	memcpy(data, src, rq->elem_size);
+
+	rq->r += rq->elem_size;
+	if (rq->r >= rq->end) {
+		rq->r = rq->base;
+	}
+	return true;
 }
 
-bool luna_queue_is_empty(struct queue *queue)
+bool luna_rq_push(struct rq *rq, const uint8_t *data)
 {
-	LUNA_ASSERT(queue);
-	return queue->nfree == queue->end;
+	LUNA_ASSERT(rq);
+	LUNA_ASSERT(data);
+
+	if (luna_rq_is_full(rq)) {
+		return false;
+	}
+	uint8_t *dst = rq->w;
+	memcpy(dst, data, rq->elem_size);
+
+	rq->w += rq->elem_size;
+	if (rq->w >= rq->end) {
+		rq->w = rq->base;
+	}
+	return true;
 }
 
-bool luna_queue_is_full(struct queue *queue)
+bool luna_rq_is_empty(const struct rq *rq)
 {
-	LUNA_ASSERT(queue);
-	return queue->nfree == 0;
+	LUNA_ASSERT(rq);
+	return rq->r == rq->w;
+}
+
+bool luna_rq_is_full(const struct rq *rq)
+{
+	LUNA_ASSERT(rq);
+	uint8_t *w = rq->w + rq->elem_size;
+	if (w >= rq->end) {
+		w = rq->base;
+	}
+	return w == rq->r;
 }
 
 #endif
